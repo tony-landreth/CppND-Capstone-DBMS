ce8eb92edef8fdc1bd377d0c19cc660db9bf5ae4
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-01-30 12:36:21 -0500

    Handles commas in CSV data

--- src/FileScan.cpp
+++ src/FileScan.cpp
@@ -11,7 +11,7 @@ FileScan::FileScan(std::string tableName)
 std::vector<std::string> FileScan::next() {
   std::vector<std::string> cols;
   std::string row;
-  std::vector<std::string> data;
+  std::vector<std::string> result;
 
   if(fileStream_) {
     getline(fileStream_, row);
@@ -25,17 +25,43 @@ std::vector<std::string> FileScan::next() {
     }
 
     // TODO: Merge on quotation marks
+    // If an element has ", concat it with the next element
+    // If the next element does not have a ", concat the previous concat with the next element
+    std::string quoted_str;
+    int size = cols.size();
+    std::vector<std::string> result;
 
-    int numCols = cols.size() - 1;
-    std::string lastEntry = cols.back();
+    for(int i = 0; i < size; i++) {
+      if(cols[i].find("\"") != std::string::npos) {
+
+        quoted_str = cols[i];
+        i++;
+
+        while(true) {
+          quoted_str = quoted_str + "," + cols[i];
+          if(cols[i].find("\"") != std::string::npos) {
+            break;
+          } else {
+            i++;
+          }
+        }
+        result.push_back(quoted_str);
+      } else {
+        result.push_back(cols[i]);
+      }
+    }
 
     // Remove \r from last entry in the row
-    lastEntry.erase(lastEntry.size() - 1);
-    cols[numCols] = lastEntry;
+    std::string lastEntry = result.back();
+    if(lastEntry.find("\r") != std::string::npos) {
+      int numCols = result.size() - 1;
+      lastEntry.erase(lastEntry.size() - 1);
+      result[numCols] = lastEntry;
+    }
 
-    return cols;
+    return result;
   }
 
-  //We'll return empty string on EOF
+  //Return empty vector if there's no data
   return cols;
 }
--- test/utest_mildDBMS.cpp
+++ test/utest_mildDBMS.cpp
@@ -5,7 +5,7 @@
 class FileScanTest : public ::testing::Test {
   protected:
     //TODO: select a subset of movie.csv as test data
-    FileScan fs{"movies"};
+    FileScan fs{"test_data"};
 };
 
 TEST_F(FileScanTest, TestNext) {
@@ -13,4 +13,9 @@ TEST_F(FileScanTest, TestNext) {
   EXPECT_EQ(row[0], "movieId");
   EXPECT_EQ(row[1], "title");
   EXPECT_EQ(row[2], "genres");
+  
+  row = fs.next();
+  std::vector<std::string> expectProperCommaHandling{ "1","\"A Movie Title, With Commas, In the Title\"","Adventure|Animation|Children|Comedy|Fantasy" };
+  EXPECT_EQ(expectProperCommaHandling, row);
+
 }
