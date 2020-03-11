9fe5362dd7221ae0e09098bd0efbcd566e80892e
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-03-11 08:38:59 -0400

    comments on limitations of schema

--- src/schema_loader.cpp
+++ src/schema_loader.cpp
@@ -1,28 +1,32 @@
 #include "schema_loader.h"
 
+// Hardcode the schema until write operations are implemented.
+// Notably, the code below does not mark foreign keys and primary
+// keys as such.
+
 std::map<std::string, int> schema_loader(std::string schema_name) {
   std::map<std::string,int> schema;
 
   if(schema_name == "movies") {
-     schema = {
+        schema = {
         { "movieId", 0 },
         { "title",   1 },
         { "genres",  2 }
       };
   } else if(schema_name == "ratings") {
-     schema = {
-       { "userId", 0 },
-       { "movieId", 1 },
-       { "rating", 2 },
-       { "timestamp", 3}
-     };
+      schema = {
+        { "userId", 0 },
+        { "movieId", 1 },
+        { "rating", 2 },
+        { "timestamp", 3}
+      };
   } else if(schema_name == "test_data") {
-     schema = {
+      schema = {
         { "movieId", 0 },
         { "title",   1 },
         { "genres",  2 }
       };
-  }
+  };
 
   return schema;
 }
