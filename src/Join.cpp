67fbcafc8d96fe09d54eb54793e3c24db0c5721f
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-03-11 08:37:19 -0400

    Adds jSchema to Join

--- src/Join.cpp
+++ src/Join.cpp
@@ -5,56 +5,71 @@
 Join::Join(std::unique_ptr<Projection> r, std::unique_ptr<Projection> s, std::vector<std::string> k) : r_(std::move( r )), s_(std::move( s )), keys_(k) {
 }
 
-// TODO: Make the below work like this:
-// Every time next() is called, we'll advance 1 S record and the total length of the R records
 std::vector<std::string> Join::next() {
+  rTableSize = r_->tableSize;
+  sTableSize = s_->tableSize;
   std::vector<std::string> result;
   std::string r_key = keys_[0];
   std::string s_key = keys_[1];
 
   std::string r_table_name = r_->tableName;
   std::string s_table_name = s_->tableName;
+
+  // TODO: The schema is only useful when you haven't projected to a smaller number of columns
+  // TODO: Or expanded the number of columns via JOIN
+  // TODO: You need to be able to remap the schema with each expansion or contraction of columns
   std::map<std::string, int> r_schema = schema_loader(r_table_name);
   std::map<std::string, int> s_schema = schema_loader(s_table_name);
 
-  // TODO: reduce all of this to just s_row and r_row
-  std::vector<std::string> r_relation = r_->next();
-  std::vector<std::string> s_relation = s_->next();
+  // Rewind the S relation so that a full table scan is possible
+  s_->rewind();
 
-  std::vector<std::string> r_row = r_relation;
+  std::vector<std::string> r_row = r_->next();
+  std::vector<std::string> s_row = s_->next();
+
+  //TODO: rename these to foreign key
   int r_colID = r_schema[r_key];
   int s_colID = s_schema[s_key];
 
-  std::vector<std::string> s_row;
+  // Use these variables to determine when the foreign keys match between two tables
   std::string r_col;
   std::string s_col;
 
-  if(r_relation.size() > 0) {
+  if(r_row.size() > 0) {
     r_col = r_row[r_colID];
   }
 
-  // TODO: Figure out what this comment is talking about
-  //  Once you roll r to the point where you've got a record that fits the where clause
-  //  You then need to start rolling s
-  // When there are projections
+  // If this is the first call to next(), return title row
+  // and build jSchema
+  if(rowIdx == 0) {
+    rowIdx++;
+    std::vector<std::string> result_row;
+    std::copy(r_row.begin(), r_row.end(), std::back_inserter(result_row));
+    std::copy(s_row.begin(), s_row.end(), std::back_inserter(result_row));
+
+    // Note that you'll end up dropping keys for identically named columns in the block below.
+    // Self joins will not be supported, until column aliasing is implemented
+    for(int i = 0; i < result_row.size(); i++){
+      jSchema[result_row[i]] = i;
+    }
+
+    return result_row;
+  }
+
   if(r_col.size() > 0) {
-    while( true ) {
-      if(s_relation.size() == 0) {
-        break;
-      }
-      std::vector<std::string> s_row = s_relation;
-      if(s_row.size() > 0)
+    for(int i = 0; i < sTableSize; i++) {
+      if(s_row.size() > 0){
         s_col = s_row[s_colID];
 
-      if(r_col == s_col) {
-        std::vector<std::string> result_row;
-        std::copy(s_row.begin(), s_row.end(), std::back_inserter(result_row));
-        std::copy(r_row.begin(), r_row.end(), std::back_inserter(result_row));
+        if(r_col == s_col) {
+          std::vector<std::string> result_row;
+          std::copy(s_row.begin(), s_row.end(), std::back_inserter(result_row));
+          std::copy(r_row.begin(), r_row.end(), std::back_inserter(result_row));
 
-        return result_row;
+          return result_row;
+        }
       }
-
-      s_relation = s_->next();
+      s_row = s_->next();
     } 
   } 
 
--- src/Join.h
+++ src/Join.h
@@ -12,11 +12,18 @@ class Join : public PlanNode
   public:
     Join(const std::unique_ptr<Projection> r, const std::unique_ptr<Projection> s, const std::vector<std::string> keys);
     std::vector<std::string> next();
+    int rTableSize;
+    int sTableSize;
+    // Joining table columns affects the number of columns in the in-memory table representation
+    // which undermines the schema used as table read time. jSchema is used to produce a sort of virtual
+    // schema to represent the ordering of the joined data in memory.
+    std::map<std::string,int> jSchema;
 
   private:
     std::unique_ptr<Projection> r_;
     std::unique_ptr<Projection> s_;
     std::vector<std::string> keys_;
+    int rowIdx = 0;
 };
 
 #endif
