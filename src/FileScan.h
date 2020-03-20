5ff6eb43888b19cd51a94f2c93507d4b6df5098a
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-03-20 11:54:43 -0400

    comments FileScan

--- src/FileScan.h
+++ src/FileScan.h
@@ -8,6 +8,13 @@
 #include<map>
 #include "PlanNode.h"
 
+
+//  FileScan nodes scan data from disk and store it in memory.
+//  Each table in the database is its own file on disk.
+//  In more realistic DBMSs, only a portion of file data would be read into memory.
+//  The present design is a first-pass, so whole files are read all at once.
+//  One FileScan node opens one file.
+
 class FileScan : public PlanNode
 {
   public:
