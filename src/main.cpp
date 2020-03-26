0846646ed2fca0efc273e6223d657b71810c663d
Anthony Landreth <anthony.w.landreth@gmail.com>
2020-03-26 09:41:33 -0400

    QueryPlanner::run() returns a pointer

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,15 @@ using std::setw;
 //TODO: Use a Template Function for something
 
 int main(int argc, char** argv) {
-  // First query is: "SELECT * FROM movies;"
-  // Like this: ./mildDBMS "SELECT * FROM movies;"
-  // Second query is: ./mildDBMS "SELECT movies.title, ratings.rating FROM movies JOIN ratings ON movieId=movieId;"
-
   QueryPlanner qp = QueryPlanner(argc, argv);
-  std::vector<std::vector<std::string> > result = qp.run();
+  std::unique_ptr<std::vector<std::vector<std::string> > > result = qp.run();
 
-  for(int i = 0; i < result.size(); i++) {
-    for(int j = 0; j < result[j].size(); j++) {
-      std::string r = result[i][j];
-      int rowLength = result[i].size();
+  for(int i = 0; i < result->size(); i++) {
+    std::vector<std::vector<std::string> > row = *result.get();
+
+    for(int j = 0; j < row[j].size(); j++) {
+      std::string r = row[i][j];
+      int rowLength = row[i].size();
     
       if(i == 0){
         // Format header
